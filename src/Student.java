ea841d1f68fd50f06d9c3afde68d055a816f5ae1
qwertyuiop232 <vk07275@gmail.com>
2018-02-22 17:20:40 -0500

    added variables

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.ArrayList;
 
 /*
  * This is just the student object that holds all the student data and will contain methods to add, edit and delete marks
@@ -7,8 +7,24 @@ import java.util.*;
  */
 public class Student {
 	ArrayList<Integer> marks;
+	String first, last, number;
+	int average;
+	ArrayList<Boolean> completed;
 	
+	/*
+	 * constructor
+	 */
 	public Student() {
-		
+		marks = new ArrayList<Integer>();
+		first = "";
+		last = "";
+		number = "";
+		average = 100;
+		completed = new ArrayList<Boolean>();
 	}
+	
+	public ArrayList<Integer> getAllMarks() {
+		return marks;
+	}
+	
 }
