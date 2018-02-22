fd98b4cba2d5b2c375379d37baddd451b36f8756
qwertyuiop232 <vk07275@gmail.com>
2018-02-22 16:56:09 -0500

    added the Student object

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/*
+ * This is just the student object that holds all the student data and will contain methods to add, edit and delete marks
+ * 
+ * @author VKo232
+ */
+public class Student {
+	ArrayList<Integer> marks;
+	
+	public Student() {
+		
+	}
+}
