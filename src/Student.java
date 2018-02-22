f7a38ad8ffd2a22dadfd3280b84fea2b72f2b299
qwertyuiop232 <vk07275@gmail.com>
2018-02-22 18:01:56 -0500

    added the methods to make the object work

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 
-/*
- * This is just the student object that holds all the student data and will contain methods to add, edit and delete marks
- * 
- * @author VKo232
+/**
+ * an object that stores all the values of the student
+ * @author qwertyuiop1
  */
 public class Student {
 	ArrayList<Integer> marks;
 	String first, last, number;
-	int average;
+	double average;
 	ArrayList<Boolean> completed;
 	
 	/*
@@ -23,8 +22,132 @@ public class Student {
 		completed = new ArrayList<Boolean>();
 	}
 	
-	public ArrayList<Integer> getAllMarks() {
-		return marks;
+	/**
+	 * @param index
+	 * @return int mark at the current index
+	 */
+	public int getMark(int index) {
+		return marks.get(index);
 	}
+	/**
+	 * @return String last name 
+	 */
+	public String getLast() {
+		return last;
+	}
+	/**
+	 * @return String first name
+	 */
+	public String getFirst() {
+		return first;
+	}
+	/**
+	 * @return average already calculated
+	 */
+	public double getAverage() {
+		setAverage();
+		return average;
+	}
+	/**
+	 * @param index which is assignment number starting from 0
+	 * @return boolean of whether it is completed or not
+	 */
+	public boolean getCompleted(int index) {
+		return completed.get(index);
+	}
+	/**
+	 * sets the assignment at that index to be marked complete, or true
+	 * @param index 
+	 */
+	public void setCompleted(int index) {
+		completed.set(index, true);
+	}
+	/**
+	 * adds a new assignment that is marked incomplete
+	 */
+	public void addIncomplete() {
+		completed.add(false);
+	}
+	/**
+	 * removes an assignment
+	 * @param index for assignment to be removed
+	 */
+	public void removeAssignment(int index) {
+		completed.remove(index);
+	}
+	/**
+	 * @return String of the student number
+	 */
+	public String getStudentNumber() {
+		return number;
+	}
+	/**
+	 * adds a new mark 
+	 * @param input
+	 */
+	public void addMark(int input) {
+		marks.add(input);
+	}
+	/**
+	 * removes a mark at the given index
+	 * @param index int of the index
+	 */
+	public void removeMark(int index) {
+		marks.remove(index);
+	}
+	/**
+	 * changes the mark at the index
+	 * @param index int
+	 * @param mark int
+	 */
+	public void editMark(int index, int mark) {
+		marks.set(index, mark);
+	}
+	/**
+	 * 
+	 * @return the amount of marks there are
+	 */
+	public int getMarkSize() {
+		return marks.size();
+	}
+	/**
+	 * 
+	 * @return the amount of assignments there are
+	 */
+	public int getAssignmentSize() {
+		return completed.size();
+	}
+	/**
+	 * sets the current student average
+	 */
+	public void setAverage() {
+		int sum = 0;
+		for(int i = 0; i < marks.size(); i++) {
+			sum += marks.get(i);
+		}
+		average = sum / (double) marks.size();
+	}
+	/**
+	 * Sets the student number
+	 * @param num this is a string
+	 */
+	public void setStudentNumber(String num) {
+		number = num;
+	}
+	/**
+	 * sets first name
+	 * @param first name string
+	 */
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	/**
+	 * sets last name
+	 * @param last
+	 */
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
 	
 }
