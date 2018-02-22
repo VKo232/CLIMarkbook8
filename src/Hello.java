efd05cf6be55d7630c50cc073ad2cbca70ea15b3
qwertyuiop232 <vk07275@gmail.com>
2018-02-22 18:10:47 -0500

    trial code to test Student object

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Hello {
     public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
         System.out.print("Hello worlds");
+        ArrayList<Student> classlist = new ArrayList<Student>();
+        
+        while(true) {
+        	String line = sc.nextLine();
+        	if(line.equals("add")) {
+        		classlist.add(new Student());
+        		System.out.println(classlist.size() + "\n" + classlist.get(0).getFirst());
+        	} else {
+        		break;
+        	}
+        }
     }
 }
