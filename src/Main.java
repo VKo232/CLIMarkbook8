b93dd2586e28c570e0b199b3b7b0feb745963e73
qwertyuiop232 <vk07275@gmail.com>
2018-02-23 22:53:54 -0500

    got an intro code

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,28 @@ import java.io.*;
 
 public class Main {
 	static ArrayList<Student> classlist = new ArrayList<Student>();
-
+	static String[][] options = { 
+			{"Reports" , "Students" , "Assignments", "Forgot Account" } ,
+			{"Class report" , "Missing assignment", "At risk students" },
+			{"Student list", "Add new student", "Remove student", "Student Information", "Edit mark"}, // needs a display of total students and averages
+			{"Add assignment", "Remove assignment", "Rename assignment", "Change assignment weight"}
+	};
     public static void main(String[] args) {
-    	
-    	System.out.println("Welcome to the CLI Markbook");
+    	System.out.println("Welcome to the CLI Markbook of group 8\n");
     	do {
-    		load();
-    		addStudent();
-    		classAverage();
-    		
+    		displayOptions();
     		if(true) {
     			break;
     		}
     	} while(true);
     	
     }
+    public static String getInput() {
+    	Scanner sc = new Scanner(System.in);
+    	String input = sc.nextLine();
+    	return input;
+    }
+    
     /**
      * Calculates and displays the class average
      */
@@ -64,16 +71,110 @@ public class Main {
     }
 
     public static void displayOptions() {
+    	String input;
+    	int choice1, choice2;
+    	Scanner sc = new Scanner(System.in);
+
+    	// prints out the first set of possibilities
+    	for(int i = 0; i < options.length; i++) {
+    		System.out.println((i + 1) + " " + options[0][i]);
+    	}
+    	// takes in a user input
+    	System.out.print("Whats your choice(num): ");
+    	do {
+    		while(!sc.hasNextInt()) {
+    			sc.nextLine();
+    			System.out.println("Thats not a number");
+    		}
+    		choice1 = Integer.parseInt(sc.nextLine());
+    		if(choice1 <= options[0].length && choice1 > 0) {
+    			break;
+    		} else {
+    			System.out.println("Invalid choice");
+    		}
+    	}while(true);
     	
+    	// shows second set of possibilities
+    	System.out.println("\nHere are your options");
+      	for(int i = 0; i < options[choice1].length; i++) {
+    		System.out.println((i + 1) + " " + options[choice1][i]);
+    	}
+      	// gets user input
+    	System.out.print("Whats your choice(num): ");
+    	do {
+    		while(!sc.hasNextInt()) {
+    			sc.nextLine();
+    			System.out.println("Thats not a number");
+    		}
+    		choice2 = Integer.parseInt(sc.nextLine());
+    		if(choice2 <= options[choice1].length && choice2 > 0) {
+    			break;
+    		} else {
+    			System.out.println("Invalid choice");
+    		}
+    	}while(true);
+    	
+    	methodCall(choice1, choice2);
     }
     
-    public static void findPassword() {
-    	
+    private static void methodCall(int choice1, int choice2) {
+		int call = choice2;
+		for(int i = 1; i < choice1; i++) {
+			call += options[i].length;
+		}
+		switch(call) {
+			case 1:
+				;
+				break;
+			case 2: 
+				;
+				break;
+			case 3: 
+				;
+				break;
+			case 4: 
+				;
+				break;
+			case 5: 
+				;
+				break;
+			case 6: 
+				;
+				break;
+			case 7: 
+				;
+				break;
+			case 8: 
+				;
+				break;
+			case 9: 
+				;
+				break;
+			case 10: 
+				;
+				break;
+			case 11: 
+				;
+				break;
+			case 12: 
+				;
+				break;
+			default: break;
+		}
+		
+	}
+	public static void findPassword() {
+    	String input;
+    	while(true) {
+    		
+    		input = getInput();
+    		if(input.equalsIgnoreCase("quit")) {
+    			break;
+    		}
+    	}
     }
     
-    public static void callMethods() {
-    	
-    }
+
     
     /**
      * adds a new student initialized with name and number
@@ -100,6 +201,7 @@ public class Main {
 		input  = sc.nextLine();
 		classlist.get(classlist.size() - 1).setStudentNumber(input);
     }
+    
     public static void save(String name) {
     	try {
 			FileOutputStream fs = new FileOutputStream(name);
